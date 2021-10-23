7ae0820cdf6b59e86169619a2c3b54bc4b2d5ae0
fadhilhrdys <fadhilahardiyasaa@gmail.com>
2021-10-23 14:44:01 +0700

    fix countdown to order

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -44,6 +44,7 @@
 import Navigation from "@/components/Navigation.vue";
 import SlideShow from "@/components/SlideShow.vue";
 import Footer from "@/components/Footer.vue";
+import $ from "jquery";
 
 export default {
   name: "Shop",
@@ -51,6 +52,34 @@ export default {
     Navigation,
     SlideShow,
     Footer,
+  },
+  mounted() {
+    let countDownDate = new Date("October 23, 2021 14:39:00").getTime();
+    let countDown = setInterval(() => {
+      let dateNow = new Date().getTime();
+      let distance = countDownDate - dateNow;
+
+      // get calculation
+      // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      $("#hour").html(`${hours}`);
+      $("#minute").html(`: ${minutes}`);
+      $("#second").html(`: ${seconds}`);
+
+      // when countdown is complete
+      if ((distance == 0) | (distance < 1)) {
+        // change the element
+        $(".btn-menu-order").html(" We'll be back tomorrow, thank you ");
+        $(".timer-wrapper").html(" CLOSE ");
+        // reset the interval
+        clearInterval(countDown);
+      }
+    });
   },
 };
 </script>
