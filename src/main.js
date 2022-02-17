40e977d9420acaa1e599d5194f4058a2e166fc4f
fadhilhrdys <fadhilahardiyasaa@gmail.com>
2022-02-17 13:14:10 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import store from './store';
 
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
